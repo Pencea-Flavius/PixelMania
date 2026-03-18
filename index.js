7a8f5b12120271cdcd0d28602c7609ce5129f139
flavius <flaviuspenca@gmail.com>
2026-03-18 13:33:38 +0200

    Lab luni

--- index.js
+++ index.js
@@ -8,10 +8,29 @@ console.log("Folder index.js", __dirname);
 console.log("Folder curent (de lucru)", process.cwd());
 console.log("Cale fisier", __filename);
 
-app.get("/cale", function (req, res) {
-    res.send("<h1>Calea <b style = 'color:red'>este:</b></h1>");
+app.use("/resurse", express.static(path.join(__dirname, "resurse")));
+//orice fisier din resurse il trimite si face send file
+
+app.get("/", function (req, res) {
+    res.render("pagini/index");
+})
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+})
+app.get("/:a/:b", function (req, res) {
+    //res.send("<h1>Calea <b style = 'color:red'>este:</b></h1>");
+    res.sendFile(path.join(__dirname, "index.html"));
+    console.log(parseInt(req.params.a) + parseInt(req.params.b));
+})
+
+app.get("/cale/:a/:b", function (req, res) {
+    //res.send("<h1>Calea <b style = 'color:red'>este:</b></h1>");
+    res.send(parseInt(req.params.a) + parseInt(req.params.b));
     console.log("Am primit o cerere Get pe /cale");
 })
+
+
 
 app.get("/cale2", function (req, res) {
     res.write("<h1>Calea este:</h1>");
